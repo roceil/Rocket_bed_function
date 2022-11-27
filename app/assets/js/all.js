63fca9e03314314ef0394d4937affaa5f7c5a558
roceil <a0978006326@gmail.com>
2022-11-27 22:44:47 +0800

    deving

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -9,12 +9,17 @@ const token = `xWFBP5xYVHNDhUnNY6F5oYdQSIB3`;
 
 // DOM-全部產品列表的ul
 const allProduct_ul = document.querySelector(`#allProduct`);
+// DOM-全部產品列表的選單按鈕
+const allProductType_btn = document.querySelector("#allProductType");
 // DOM-購物車列表的ul
 const cartList_ul = document.querySelector(`#cartList`);
+// DOM-清空購物車的btn
+const clearAllCart_btn = document.querySelector(`#clearAllCart_btn`);
+
 allProductRender();
 cartListRender();
 
-// 初始化渲染全部產品
+// 函式 => 初始化渲染全部產品
 function allProductRender() {
   // * 撈取所有產品資料
   axios
@@ -40,7 +45,7 @@ function allProductRender() {
     });
 }
 
-// 初始化渲染購物車列表
+// 函式 => 初始化渲染購物車列表
 function cartListRender() {
   // * 撈取購物車列表資料
   axios
@@ -50,8 +55,8 @@ function cartListRender() {
     .then((res) => {
       let cartListStr = ``;
       res.data["carts"].map((item) => {
-        console.log(item['id']);
-        cartListStr += `<li class="flex items-center"><p class="min-w-[300px]">${item["product"]["title"]}</p><input cart_id=${item['id']} class="btn btn-hover" type="button" value="刪除商品" /></li>`;
+        console.log(1);
+        cartListStr += `<li class="flex items-center"><p class="min-w-[300px]">${item["product"]["title"]}</p><input cart_id=${item["id"]} class="btn btn-hover" type="button" value="刪除商品" /></li>`;
         cartList_ul.innerHTML = cartListStr;
       });
     })
@@ -60,19 +65,7 @@ function cartListRender() {
     });
 }
 
-// function cartListRender() {
-
-// }
-
-allProduct_ul.addEventListener("click", (e) => {
-  if (e.target.type === "button") {
-    console.log("點到了按鈕");
-    addToCart(e);
-  } else {
-    return;
-  }
-});
-
+// 函式 => 加入購物車
 function addToCart(e) {
   // * 撈取購物車列表資料
   axios
@@ -93,4 +86,68 @@ function addToCart(e) {
     .catch((err) => {
       console.log(err);
     });
+}
+
+//函式 => 清空購物車
+function clearAllCart() {
+  axios
+    .delete(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${route}/carts
+  `
+    )
+    .then((res) => {
+      // cartListRender() => //? 按按鈕後不會重新渲染
+      window.location.reload(); // !因為不會重新渲染，所以重新整理
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// 監聽 => 全部產品類別選擇
+allProductType_btn.addEventListener("change", (e) => {
+  // console.log(e.target.value);
+  axios
+    .get(
+      `https://livejs-api.hexschool.io/api/livejs/v1/customer/${route}/products`
+    )
+    .then((res) => {
+      let allProductStr = ``;
+      res.data["products"].map((item) => {
+        console.log(item["category"]);
+        if (e.target.value === item["category"]) {
+          allProductStr += `<li class="flex items-center">
+          <p class="min-w-[300px]">${item["title"]}</p>
+          <input
+          id=${item["id"]}
+          class="btn btn-hover addToCartBtn"
+          type="button"
+          value="加入購物車"
+        />`;
+          allProduct_ul.innerHTML = allProductStr;
+        } else if (e.target.value === "全部") {
+          allProductRender();
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+// 監聽 => 加入購物車按鈕
+allProduct_ul.addEventListener("click", (e) => {
+  if (e.target.type === "button") {
+    console.log("點到了按鈕");
+    addToCart(e);
+  } else {
+    return;
+  }
+});
+// 監聽 => 清空購物車按鈕
+clearAllCart_btn.addEventListener("click", (e) => {
+  clearAllCart();
+});
+
+function productSelect() {
+  // * 撈取所有產品資料
 }
