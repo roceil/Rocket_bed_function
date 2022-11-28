cd1e494c754585dcbc8b7412553835c744ec6127
roceil <a0978006326@gmail.com>
2022-11-28 15:39:42 +0800

    Update 2022-11-28T07:39:41.054Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,19 +3,19 @@
 // ! API-route
 var route = "rocket-frank"; // ! API-token
 
-var token = "xWFBP5xYVHNDhUnNY6F5oYdQSIB3"; // // ! 暫存產品列表data
-// let allProductData = [];
-// // ! 暫存購物車列表data
-// let cartListData = [];
-// DOM-全部產品列表的ul
+var token = "xWFBP5xYVHNDhUnNY6F5oYdQSIB3"; // DOM => 全部產品列表的ul
 
-var allProduct_ul = document.querySelector("#allProduct"); // DOM-全部產品列表的選單按鈕
+var allProduct_ul = document.querySelector("#allProduct"); // DOM => 全部產品列表的選單按鈕
 
-var allProductType_btn = document.querySelector("#allProductType"); // DOM-購物車列表的ul
+var allProductType_btn = document.querySelector("#allProductType"); // DOM => 購物車列表的ul
 
-var cartList_ul = document.querySelector("#cartList"); // DOM-清空購物車的btn
+var cartList_ul = document.querySelector("#cartList"); // DOM => 清空購物車的btn
 
-var clearAllCart_btn = document.querySelector("#clearAllCart_btn");
+var clearAllCart_btn = document.querySelector("#clearAllCart_btn"); // DOM => 刪除購物車商品的btn
+
+var deleteProduct_btn = document.querySelector("#deleteProductBtn"); // DOM => 送出訂單的form
+
+var orderSubmit_form = document.querySelector("#orderSubmit");
 allProductRender();
 cartListRender(); // 函式 => 初始化渲染全部產品
 
@@ -24,7 +24,7 @@ function allProductRender() {
   axios.get("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/products")).then(function (res) {
     var allProductStr = "";
     res.data["products"].map(function (item) {
-      allProductStr += "<li class=\"flex items-center\">\n        <p class=\"min-w-[300px]\">".concat(item["title"], "</p>\n        <input\n        id=").concat(item["id"], "\n        class=\"btn btn-hover addToCartBtn\"\n        type=\"button\"\n        value=\"\u52A0\u5165\u8CFC\u7269\u8ECA\"\n      />");
+      allProductStr += "<li class=\"flex items-center\">\n        <p class=\"min-w-[300px]\">".concat(item["title"], "</p>\n        <input\n        data-id=").concat(item["id"], "\n        class=\"btn btn-hover addToCartBtn\"\n        type=\"button\"\n        value=\"\u52A0\u5165\u8CFC\u7269\u8ECA\"\n      />");
       allProduct_ul.innerHTML = allProductStr;
     });
   })["catch"](function (err) {
@@ -36,12 +36,16 @@ function allProductRender() {
 function cartListRender() {
   // * 撈取購物車列表資料
   axios.get("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/carts")).then(function (res) {
-    var cartListStr = "";
-    res.data["carts"].map(function (item) {
-      console.log(1);
-      cartListStr += "<li class=\"flex items-center\"><p class=\"min-w-[300px]\">".concat(item["product"]["title"], "</p><input cart_id=").concat(item["id"], " class=\"btn btn-hover\" type=\"button\" value=\"\u522A\u9664\u5546\u54C1\" /></li>");
+    if (res.data["carts"].length === 0) {
+      var cartListStr = "<li class=\"text-xl font-bold\">\u8CFC\u7269\u8ECA\u6C92\u6709\u5546\u54C1</li>";
       cartList_ul.innerHTML = cartListStr;
-    });
+    } else {
+      var _cartListStr = "";
+      res.data["carts"].map(function (item) {
+        _cartListStr += "<li class=\"flex items-center\"><p class=\"min-w-[300px]\">".concat(item["product"]["title"], "</p><input id=\"deleteProductBtn\" cart-id=").concat(item["id"], " class=\"btn btn-hover\" type=\"button\" value=\"\u522A\u9664\u5546\u54C1\" /></li>");
+        cartList_ul.innerHTML = _cartListStr;
+      });
+    }
   })["catch"](function (err) {
     console.log(err);
   });
@@ -52,11 +56,11 @@ function addToCart(e) {
   // * 撈取購物車列表資料
   axios.post("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/carts\n"), {
     data: {
-      productId: "".concat(e.target.id),
+      productId: "".concat(e.target.getAttribute("data-id")),
       quantity: 5
     }
   }).then(function (res) {
-    console.log(e.target.id);
+    alert("此商品已加入購物車");
     cartListRender();
   })["catch"](function (err) {
     console.log(err);
@@ -66,10 +70,35 @@ function addToCart(e) {
 
 function clearAllCart() {
   axios["delete"]("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/carts\n  ")).then(function (res) {
-    // cartListRender() => //? 按按鈕後不會重新渲染
-    window.location.reload(); // !因為不會重新渲染，所以重新整理
+    cartListRender();
+    alert("購物車已清空");
   })["catch"](function (err) {
     console.log(err);
+  });
+} //函式 => 送出訂單
+
+
+function orderSubmit(forms) {
+  axios.post("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/orders"), {
+    data: {
+      user: {
+        name: forms[0].value,
+        tel: forms[1].value,
+        email: forms[2].value,
+        address: forms[3].value,
+        payment: forms[4].value
+      }
+    }
+  }).then(function (res) {
+    for (var i = 0; i < 4; i++) {
+      forms[i].value = "";
+      forms[4].value = "none";
+    }
+
+    cartListRender();
+    alert("訂單建立成功");
+  })["catch"](function (err) {
+    alert(err.response.data.message);
   });
 } // 監聽 => 全部產品類別選擇
 
@@ -95,7 +124,6 @@ allProductType_btn.addEventListener("change", function (e) {
 
 allProduct_ul.addEventListener("click", function (e) {
   if (e.target.type === "button") {
-    console.log("點到了按鈕");
     addToCart(e);
   } else {
     return;
@@ -104,8 +132,35 @@ allProduct_ul.addEventListener("click", function (e) {
 
 clearAllCart_btn.addEventListener("click", function (e) {
   clearAllCart();
-});
+}); // 監聽 => 刪除購物車商品按鈕
 
-function productSelect() {// * 撈取所有產品資料
-}
+cartList_ul.addEventListener("click", function (e) {
+  if (e.target.type === "button") {
+    axios["delete"]("https://livejs-api.hexschool.io/api/livejs/v1/customer/".concat(route, "/carts/").concat(e.target.getAttribute("cart-id"))).then(function (res) {
+      console.log(res.data);
+      cartListRender();
+      alert("此商品已刪除");
+    })["catch"](function (err) {
+      console.log(err);
+    });
+  } else {
+    return;
+  }
+}); // 監聽 => 送出訂單的各項資料
+
+orderSubmit_form.addEventListener("click", function (e) {
+  e.preventDefault();
+  var forms = document.forms[0];
+
+  if (e.target.value === "送出訂單") {
+    for (var i = 0; i < forms.length; i++) {
+      if (forms[i].value === "" || forms[4].value === "none") {
+        alert("請填寫每筆資料");
+        return;
+      }
+    }
+
+    orderSubmit(forms);
+  }
+});
 //# sourceMappingURL=all.js.map
